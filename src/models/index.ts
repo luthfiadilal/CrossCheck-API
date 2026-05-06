4349f48f639125aacdf8c6b7dc23319d476d65dc
Luthfi Adilal Mahbub <98248550+luthfiadilal@users.noreply.github.com>
2026-05-06 20:30:48 +0700

    hapus foregen key

--- src/models/index.ts
+++ src/models/index.ts
@@ -9,32 +9,32 @@ import TrMonitoringPhoto from "./tr-monitoring-photo.model";
 // ===================== Associations =====================
 
 // MsUser self-referencing: atasan -> bawahan
-MsUser.hasMany(MsUser, { foreignKey: "parent_user_id", as: "subordinates" });
-MsUser.belongsTo(MsUser, { foreignKey: "parent_user_id", as: "superior" });
+MsUser.hasMany(MsUser, { foreignKey: "parent_user_id", as: "subordinates", constraints: false });
+MsUser.belongsTo(MsUser, { foreignKey: "parent_user_id", as: "superior", constraints: false });
 
 // MsUser (mandor) -> TrMonitoringLog
-MsUser.hasMany(TrMonitoringLog, { foreignKey: "mandor_id", as: "monitoringLogs" });
-TrMonitoringLog.belongsTo(MsUser, { foreignKey: "mandor_id", as: "mandor" });
+MsUser.hasMany(TrMonitoringLog, { foreignKey: "mandor_id", as: "monitoringLogs", constraints: false });
+TrMonitoringLog.belongsTo(MsUser, { foreignKey: "mandor_id", as: "mandor", constraints: false });
 
 // TrMonitoringLog -> TrMonitoringDetail (one-to-many)
-TrMonitoringLog.hasMany(TrMonitoringDetail, { foreignKey: "log_id", as: "details" });
-TrMonitoringDetail.belongsTo(TrMonitoringLog, { foreignKey: "log_id", as: "monitoringLog" });
+TrMonitoringLog.hasMany(TrMonitoringDetail, { foreignKey: "log_id", as: "details", constraints: false });
+TrMonitoringDetail.belongsTo(TrMonitoringLog, { foreignKey: "log_id", as: "monitoringLog", constraints: false });
 
 // MsTaskType -> TrMonitoringDetail
-MsTaskType.hasMany(TrMonitoringDetail, { foreignKey: "task_type_id", as: "monitoringDetails" });
-TrMonitoringDetail.belongsTo(MsTaskType, { foreignKey: "task_type_id", as: "taskType" });
+MsTaskType.hasMany(TrMonitoringDetail, { foreignKey: "task_type_id", as: "monitoringDetails", constraints: false });
+TrMonitoringDetail.belongsTo(MsTaskType, { foreignKey: "task_type_id", as: "taskType", constraints: false });
 
 // TrMonitoringLog -> TrApprovalHistory (one-to-many)
-TrMonitoringLog.hasMany(TrApprovalHistory, { foreignKey: "log_id", as: "approvalHistories" });
-TrApprovalHistory.belongsTo(TrMonitoringLog, { foreignKey: "log_id", as: "monitoringLog" });
+TrMonitoringLog.hasMany(TrApprovalHistory, { foreignKey: "log_id", as: "approvalHistories", constraints: false });
+TrApprovalHistory.belongsTo(TrMonitoringLog, { foreignKey: "log_id", as: "monitoringLog", constraints: false });
 
 // MsUser (approver) -> TrApprovalHistory
-MsUser.hasMany(TrApprovalHistory, { foreignKey: "approver_id", as: "approvals" });
-TrApprovalHistory.belongsTo(MsUser, { foreignKey: "approver_id", as: "approver" });
+MsUser.hasMany(TrApprovalHistory, { foreignKey: "approver_id", as: "approvals", constraints: false });
+TrApprovalHistory.belongsTo(MsUser, { foreignKey: "approver_id", as: "approver", constraints: false });
 
 // TrMonitoringDetail -> TrMonitoringPhoto (one-to-many)
-TrMonitoringDetail.hasMany(TrMonitoringPhoto, { foreignKey: "detail_id", as: "photos" });
-TrMonitoringPhoto.belongsTo(TrMonitoringDetail, { foreignKey: "detail_id", as: "detail" });
+TrMonitoringDetail.hasMany(TrMonitoringPhoto, { foreignKey: "detail_id", as: "photos", constraints: false });
+TrMonitoringPhoto.belongsTo(TrMonitoringDetail, { foreignKey: "detail_id", as: "detail", constraints: false });
 
 // ========================================================
 
